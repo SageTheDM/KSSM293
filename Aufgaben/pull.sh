a712e543e925e1efd3493ad613ae2e37cdef5330
sageTheDM <info@photofuel.tech>
2025-02-25 16:11:54 +0100

    updates

---
+++ Aufgaben/pull.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# put this file into Aufgaben/
+
+git clone git@github.com:ims-kss/js-wuerfel-poker-SageTheDM.git diceRoulette/
+cd diceRoulette
+sudo rm -rf .git 
+sudo rm -rf README.md
